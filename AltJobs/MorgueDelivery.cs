ea4f406ddc56547f48f462929e11deddcfc29433
Emma Davenport <davenport.physics@gmail.com>
2019-04-05 15:40:36 -0500

    Fixed timer issue and reduced current time limites by 10 seconds

--- AltJobs/MorgueDelivery.cs
+++ AltJobs/MorgueDelivery.cs
@@ -25,11 +25,11 @@ namespace AltJobs
         private int mission_idx  = 0;
         private List<Mission> missions = new List<Mission>()
         {
-            new Mission("blackmarket", 2500, 220*1000, new Vector3(3788.968f, 4462.44f, 5.27f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/blackmarket-hearts.mp3", "sounds/AltJobs/blackmarket-brain.mp3", "sounds/AltJobs/SandyBM1.mp3" }, new List<string>(){ "Human heart", "Human brain", "Human bones" })),
-            new Mission("docks", 1250, 105*1000, new Vector3(166.0794f, -3299.002f, 5.28f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/docks-juice.mp3", "sounds/AltJobs/docks-chemicals.mp3", "sounds/AltJobs/SandyD1.mp3" }, new List<string>(){ "Human juice", "Questionable Chemicals", "Embalming Fluid" })),
-            new Mission("airport", 1000 , 85*1000, new Vector3(-1022.813f, -2706.281f, 12.607f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/airport-teeth.mp3", "sounds/AltJobs/airport-liver.mp3" }, new List<string>(){ "Human Bones", "Human Teeth", "Human Liver" })),
-            new Mission("humanelabs", 1000 , 240*1000, new Vector3(3568.353f, 3664.556f, 33.20224f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/humanelabs-brain.mp3" }, new List<string>(){ "Human juice", "Questionable Chemicals", "Human brain" })),
-            new Mission("nudistcolony", 4000 , 300*1000, new Vector3(-1097.769f, 4945.581f, 217.5335f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/nudistcolony-bones.mp3", "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/nudistcolony-juice.mp3" }, new List<string>(){ "Human Bones", "Human Teeth", "Human juice" }))
+            new Mission("blackmarket", 2500, 210*1000, new Vector3(3788.968f, 4462.44f, 5.27f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/blackmarket-hearts.mp3", "sounds/AltJobs/blackmarket-brain.mp3", "sounds/AltJobs/SandyBM1.mp3" }, new List<string>(){ "Human heart", "Human brain", "Human bones" })),
+            new Mission("docks", 1250, 95*1000, new Vector3(166.0794f, -3299.002f, 5.28f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/docks-juice.mp3", "sounds/AltJobs/docks-chemicals.mp3", "sounds/AltJobs/SandyD1.mp3" }, new List<string>(){ "Human juice", "Questionable Chemicals", "Embalming Fluid" })),
+            new Mission("airport", 1000 , 75*1000, new Vector3(-1022.813f, -2706.281f, 12.607f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/airport-teeth.mp3", "sounds/AltJobs/airport-liver.mp3" }, new List<string>(){ "Human Bones", "Human Teeth", "Human Liver" })),
+            new Mission("humanelabs", 2500 , 230*1000, new Vector3(3568.353f, 3664.556f, 33.20224f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/humanelabs-brain.mp3" }, new List<string>(){ "Human juice", "Questionable Chemicals", "Human brain" })),
+            new Mission("nudistcolony", 3500 , 290*1000, new Vector3(-1097.769f, 4945.581f, 217.5335f), new ItemIdentifiers(new List<string>(){ "sounds/AltJobs/nudistcolony-bones.mp3", "sounds/AltJobs/SandyA1.mp3", "sounds/AltJobs/nudistcolony-juice.mp3" }, new List<string>(){ "Human Bones", "Human Teeth", "Human juice" }))
         };
 
         public MorgueDelivery()
@@ -94,7 +94,7 @@ namespace AltJobs
         {
             if (this.distance_to_marker <= 3 && !this.job_started && API.IsControlJustPressed(1, 18))
             {
-                if (API.IsPedInAnyVehicle(Game.PlayerPed.Handle, false))
+                if (API.IsPedInAnyVehicle(Game.PlayerPed.Handle, false) && API.GetPedInVehicleSeat(API.GetVehiclePedIsIn(Game.PlayerPed.Handle, false), -1) == Game.PlayerPed.Handle)
                 {
                     StartJob();
                 } else
@@ -146,14 +146,14 @@ namespace AltJobs
         {
             this.mission_name = mission_name;
             this.base_pay     = base_pay;
-            this.base_timer = base_timer;
+            this.base_timer   = base_timer;
             this.end_point    = end_point;
             this.items        = items;
         }
 
         public async void RunJob()
         {
-            InitJob();
+            await InitJob();
             while (!is_job_done)
             {
                 await Delay(5);
@@ -164,7 +164,7 @@ namespace AltJobs
             }
         }
 
-        public void InitJob()
+        public async Task InitJob()
         {
             this.is_job_done        = false;
             this.mission_item       = rand.Next(this.items.item_names.Count);
@@ -174,8 +174,8 @@ namespace AltJobs
             TriggerEvent("addItem", this.mission_item_name, 1, true);
 
             PlayMissionDialog();
+            await FadeOut();
             SetTimer();
-            FadeOut();
             API.SetNewWaypoint(this.end_point[0], this.end_point[1]);
         }
 
@@ -184,7 +184,7 @@ namespace AltJobs
             Exports["PlayExternalSounds"].PlaySound(this.mission_sound_file, .3f);
         }
 
-        private async void FadeOut()
+        private async Task FadeOut()
         {
             API.DoScreenFadeOut(1000);
             await Delay(7500);
